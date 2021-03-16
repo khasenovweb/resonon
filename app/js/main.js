7ffadbbd7cc3d28e65a17a4af42c85fbe8d6da27
khasenovweb <hasenovkirill93@gmail.com>
2021-03-16 21:11:36 +0500

    dfjdfkjh

--- app/js/main.js
+++ app/js/main.js
@@ -6,6 +6,49 @@ $(document).ready(function(){
         navText: ["<img src=\"/img/slider-arrow-1.svg\">","<img src=\"/img/slider-arrow-2.svg\" >"],
     });
 
+    //Навигация посередине изображения в слайдере
+    $('.mobile__nav__submenu').slideUp();
+    $('.has-children').click(function(){
+        if ($(event.target).closest(".mobile__nav__submenu").length) return;
+        $(this).find('.mobile__nav__submenu').slideToggle(300);
+        $(this).toggleClass('active');
+    });
+    //END Навигация посередине изображения в слайдере
+
+    $('[data-hamburger="showhide"]').click(function(){
+        if( $('[data-mobile-nav]').hasClass('active') ) {
+            $('[data-mobile-nav]').removeClass('active')
+            $('body').removeClass('hidden');
+            setTimeout(function(){
+                $('[data-mobile-nav]').hide();
+            },300);
+        }else {
+            $('[data-mobile-nav]').show();
+            setTimeout(function(){
+                $('[data-mobile-nav]').addClass('active')
+                $('body').addClass('hidden');
+            },1);
+            
+        }
+    });
+
+
+    $('.modal__wrapper').click(function(){
+        var thisval = $(this);
+        if ($(event.target).closest(".modal").length) return;
+        $(this).removeClass('active');
+        setTimeout(function(){
+            thisval.hide();
+        },300);
+    });
+    $('[data-modal-show]').click(function(){
+        var id = $(this).attr('data-modal-show');
+        $('[data-modal="'+id+'"]').css('display', 'flex');
+        setTimeout(function(){
+            $('[data-modal="'+id+'"]').addClass('active');
+        },1);
+        
+    });
 
     //Навигация посередине изображения в слайдере
     function slider_nav(){
